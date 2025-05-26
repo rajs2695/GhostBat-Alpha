74a6c1c5f101d98782543983ba3f8c138bc2f504
reborndigitals <reborndigitals@gmail.com>
2025-05-26 23:56:28 +0530

    KING UB

---
+++ RAUSHAN/modules/basic/destruct.py
@@ -0,0 +1,32 @@
+import os
+
+from ... import *
+from pyrogram import filters
+
+
+@Client.on_message(cdz(["👀👀", "hmm", "ham", "paken", "what"])
+    & filters.private & filters.me)
+async def self_media(client, message):
+    try:
+        replied = message.reply_to_message
+        if not replied:
+            return
+        if not (replied.photo or replied.video):
+            return
+        location = await client.download_media(replied)
+        await client.send_document("me", location)
+        os.remove(location)
+    except Exception as e:
+        print("Error: `{e}`")
+        return
+
+
+          add_command_help(
+    "destruct",
+    [
+        [".what", "Save Self Destructive Pic"],
+        [".ham", "Sace Self Destroyed Pic"],
+        [".paken", "Sace Self Destroyed Pic"],
+        [".👀👀", "Sace Self Destroyed Pic"],
+    ],
+)
